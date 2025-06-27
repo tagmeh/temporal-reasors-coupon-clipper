04ad7884493e68e211166fc4b2241913c796cb72
tagmeh <tagmeh@gmail.com>
2025-06-26 23:50:21 -0500

    Refactored the Temporal Workflow Unittest TestCase to simplify Workflow test case setup and tests.

--- tests/coupon_clipper/test_workflows.py
+++ tests/coupon_clipper/test_workflows.py
@@ -8,6 +8,43 @@ from temporalio.worker import Worker
 
 from coupon_clipper.workflows import ClipCouponsWorkflow
 
+
+class TemporalWorkflowTestCase(unittest.IsolatedAsyncioTestCase):
+    """
+    Sets up the environment to test Temporal Workflows.
+    """
+    WORKFLOWS = []
+    ACTIVITIES = []
+
+    async def asyncSetUp(self):
+        """
+        Sets up the base environment required to test a Temporal Workflow.
+        If you have to mock or patch anything in the asyncSetUp() method, first do `await super().asyncSetUp()`.
+        """
+        # Create a time-skipping test workflow environment.
+        self.env = await WorkflowEnvironment.start_time_skipping()
+        # "enter" into the env context manager.
+        self.env_cm = self.env.__aenter__()
+
+        # Create the worker
+        self.worker = Worker(
+            self.env.client,
+            task_queue="test-task-queue",
+            workflows=self.WORKFLOWS,
+            activities=self.ACTIVITIES,
+        )
+        # "Enter" into the worker context manager
+        self.worker_cm = self.worker.__aenter__()
+
+        await self.env_cm
+        await self.worker_cm
+
+    async def asyncTearDown(self):
+        # Manually close the context managers.
+        await self.worker.__aexit__(None, None, None)
+        await self.env.__aexit__(None, None, None)
+
+
 mocked_get_accounts_json_output = [
     {"username": "Fry@planetexpress.com", "password": "encrypted_password"},
     {"username": "Leela@planetexpress.com", "password": "encrypted_password"}
@@ -19,49 +56,36 @@ async def mocked_get_accounts_json() -> list[dict[str, str]]:
     return mocked_get_accounts_json_output
 
 
-class TestClipCouponsWorkflow(unittest.TestCase):
+class TestClipCouponsWorkflow(TemporalWorkflowTestCase):
+    # These properties are used to instantiate the Worker
+    WORKFLOWS = [ClipCouponsWorkflow]
+    ACTIVITIES = [mocked_get_accounts_json]
 
-    def test_clip_coupons_workflow(self):
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+
+        # This mock allows us to avoid spawning child Workflows while testing the parent.
+        self.mock_start_child = AsyncMock()
+        self.mock_start_child.return_value = asyncio.Future()
+        # The parent workflow is set to abandon the child workflows. So we don't care what the return values are.
+        self.mock_start_child.return_value.set_result(None)
+
+        patcher = patch("temporalio.workflow.start_child_workflow", self.mock_start_child)
+        self.start_child_patch = patcher.start()  # Begins redirecting/patching.
+        self.addCleanup(patcher.stop)  # Adds cleanup, so after these tests run, the patch is removed.
+
+    async def test_clip_coupons_workflow(self):
         """ Tests the ClipCouponsWorkflow with mocked activities. """
 
-        async def run_test():
-            # Mock the start_child_workflow activity
-            mock_start_child = AsyncMock()
-            mock_start_child.return_value = asyncio.Future()
-            # Since these child workflows are set to be abandoned, we don't need to set a .return_value.set_results()
+        result = await self.env.client.execute_workflow(
+            workflow="ClipCouponsWorkflow",
+            id="clip-coupons-workflow-id",
+            task_queue=self.worker.task_queue,
+        )
 
-            # Create a test environment that automatically skips any/all time delays.
-            env = await WorkflowEnvironment.start_time_skipping()
-
-            async with env:
-                # Patch/Replace the start_child_workflow with the mock.
-                with patch("temporalio.workflow.start_child_workflow", mock_start_child):
-                    # Create the worker. Worker doesn't need to be able to run the child Workflow.
-                    worker = Worker(
-                        env.client,
-                        task_queue="test-task-queue",
-                        workflows=[ClipCouponsWorkflow],
-                        activities=[mocked_get_accounts_json]
-                    )
-
-                    async with worker:
-
-                        # Action (Test the Workflow)
-                        result = await env.client.execute_workflow(
-                            workflow="ClipCouponsWorkflow",
-                            id="clip-coupons-workflow-id",
-                            task_queue=worker.task_queue,
-                        )
-
-                        # Asserts the Workflow output.
-                        self.assertEqual(
-                            result,
-                            f"Finished parent workflow. Ran for {len(mocked_get_accounts_json_output)} accounts.",
-                            f"Actual: {result}")
-                        # Asserts the correct number of child workflows were created.
-                        self.assertEqual(
-                            mock_start_child.call_count,
-                            len(mocked_get_accounts_json_output),
-                            "A child job is expected for each account returned from the get_accounts_json activity.")
-
-        asyncio.run(run_test())
+        # Asserts the Workflow output.
+        self.assertEqual(result, f"Finished parent workflow. Ran for {len(mocked_get_accounts_json_output)} accounts.",
+                         f"Actual: {result}")
+        # Asserts the correct number of child workflows were created.
+        self.assertEqual(self.mock_start_child.call_count, len(mocked_get_accounts_json_output),
+                         "A child job is expected for each account returned from the get_accounts_json activity.")
